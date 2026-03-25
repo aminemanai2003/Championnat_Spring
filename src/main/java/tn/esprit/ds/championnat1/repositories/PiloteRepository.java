8195ad65700bec9c1df7ad9fbd08f1b57e45fa11
aminemanai2003 <amine.manai@esprit.tn>
2026-03-25 21:14:13 +0100

    Homework Spring Data JPA Affectations

--- src/main/java/tn/esprit/ds/championnat1/repositories/PiloteRepository.java
+++ src/main/java/tn/esprit/ds/championnat1/repositories/PiloteRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tn.esprit.ds.championnat1.entities.Pilote;
 
+import java.util.Optional;
+
 @Repository
 public interface PiloteRepository extends JpaRepository<Pilote, Long> {
+	Optional<Pilote> findByLibelleP(String libelleP);
 }
--- src/main/java/tn/esprit/ds/championnat1/repositories/SponsorRepository.java
+++ src/main/java/tn/esprit/ds/championnat1/repositories/SponsorRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import tn.esprit.ds.championnat1.entities.Sponsor;
 
+import java.util.Optional;
+
 public interface SponsorRepository extends JpaRepository<Sponsor,Long> {
+	Optional<Sponsor> findByNomAndPays(String nom, String pays);
 
 }
